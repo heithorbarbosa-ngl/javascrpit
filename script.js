050dc4cb702679633bb0ef42bce7be1d18e91296
heithorbarbosa-ngl <heithor_barbosa@estudante.sesisenai.org.br>
2025-09-10 19:35:12 -0300

    teste2

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = window.prompt("escreva seu nome:");
+let idade = window.prompt("escreva sua idade:")
+let altura = window.prompt("escreva sua altura:")
+
+console.log("seu nome é ", nome, ",vc tem ", idade, " anos, e ", altura, " metros de altura")
