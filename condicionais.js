188bc9490e77ce27dbc22e2c9c64fb46459fd3ac
heithorbarbosa-ngl <heithor_barbosa@estudante.sesisenai.org.br>
2025-09-11 00:17:32 +0000

    atividades

---
+++ condicionais.js
@@ -0,0 +1,6 @@
+let nota = 0
+if (nota >= 7) {
+    console.log("aprovado")
+} else ("reprovado")
+
+console.log (typeof nota)
---
+++ exercicio-pratico.js
@@ -0,0 +1,11 @@
+let nome = "heithor"
+let sobrenome = "borchardt"
+let idade = 17
+let altura = 1.84
+let gostaDeProgramar = true
+
+console.log(`seu nome é ${nome}`)
+console.log(`seu sobrenome é ${sobrenome}`)
+console.log(`sua idade é ${idade}`)
+console.log(`sua altura é de ${altura}`)
+console.log(`gosta de programar? ${gostaDeProgramar}`)
